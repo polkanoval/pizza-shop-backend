830cabba4ac21199265524b736b2ff801ac9d584
Полканов Александр Валерьевич <alexander.polkanov@baikalelectronics.ru>
2025-12-28 14:52:46 +0300

    add yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
