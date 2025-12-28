a1e2407e937638c08573bf795e6850eaaed07c1c
Полканов Александр Валерьевич <alexander.polkanov@baikalelectronics.ru>
2025-12-28 14:44:12 +0300

    add yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(l!dw$3&9&#wno2e0q^a4aiszgi5**er*nb-s6#&(hk2$iwn#*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-if-not-found')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
